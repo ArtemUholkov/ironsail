f5c93ed0a15ddf400b0e7d2f54f9b2a442d24e1f
Artem <artem.ugolkov@ukr.net>
2024-04-17 13:13:14 +0300

    fixes, pages

---
+++ js/influ.js
@@ -0,0 +1,18 @@
+const iinflu = document.querySelector('.im_influ_wrapper');
+const ineedinflu = document.querySelector('.i_need_influ_wrapper');
+const butswrapper = document.querySelector('.im_im_items_wrapper');
+const iinflubut = document.querySelector('.im_im_influ');
+const ineedinflubut = document.querySelector('.im_need_inf');
+
+iinflubut.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  iinflu.classList.add('show_block');
+  butswrapper.classList.add('hide_block');
+});
+ineedinflubut.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  ineedinflu.classList.add('show_block');
+  butswrapper.classList.add('hide_block');
+});
